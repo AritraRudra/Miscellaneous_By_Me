4a992837db13c877da2bf41fecb086dff762d6a6
AritraRudra <aritrarudra@gmail.com>
2016-10-11 22:25:38 +0530

    Updated QueueInterface class with comments

--- Data_Structures_1/src/queue/QueueInterface.java
+++ Data_Structures_1/src/queue/QueueInterface.java
@@ -2,33 +2,52 @@ package queue;
 
 // https://www.cs.cmu.edu/~adamchik/15-121/lectures/Stacks%20and%20Queues/Stacks%20and%20Queues.html
 // Each of the above basic operations must run at constant time O(1).
-interface QueueInterface<T>{	
+interface QueueInterface<T>{
 
 	/**
      * Add the item to the queue.
+     * @throws Exception
+	 *             if this queue is full.
      */
-	public void enqueue(T item);
+	public void enqueue(T item) throws Exception;
 
 	/**
 	 * Removes and returns the item on this queue that was least recently added.
 	 *
 	 * @return the item on this queue that was least recently added
-	 * @throws NoSuchElementException
-	 *             if this queue is empty
+	 * @throws Exception
+	 *             if this queue is empty.
 	 */
-	public T dequeue();
+	public T dequeue() throws Exception;
 	
 	/**
      * Returns the item least recently added to this queue.
      *
-     * @return the item least recently added to this queue
-     * @throws NoSuchElementException if this queue is empty
+     * @return the item least recently added to this queue.
+     * @throws Exception if this queue is empty.
      */
-	public T peek();
+	public T peek() throws Exception;
 
+	/**
+	 * Returns true if this queue is full.
+	 *
+	 * @return {@code true} if this queue is full; {@code false} otherwise.
+	 */
+	public boolean isFull();
+
+	/**
+	 * Returns true if this queue is empty.
+	 *
+	 * @return {@code true} if this queue is empty; {@code false} otherwise
+	 */
 	public boolean isEmpty();
 	
-	public int size();
+	/**
+	 * Returns the number of items in this queue.
+	 *
+	 * @return the number of items in this queue.
+	 */
+	public int getNumberOfItems();
 	
 	public void clear();
 }
