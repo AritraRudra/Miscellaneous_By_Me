1bac8dd69358f71fb54a7711bd3b788d435b533d
AritraRudra <aritrarudra@gmail.com>
2016-10-30 20:00:03 +0530

    Updated StackDemo.java class.

--- Data_Structures_1/src/stack/StackDemo.java
+++ Data_Structures_1/src/stack/StackDemo.java
@@ -4,33 +4,33 @@ public class StackDemo {
 	public static void main(String[] args) {
 		final int stackSize = 5;
 		Stack<String> stack = new Stack<String>(stackSize);
-		
-		try{
+
+		try {
 			stack.push("A");
-			System.out.println("Item pushed into stack : "+stack.peek());
+			System.out.println("Item pushed into stack : " + stack.peek());
 			stack.push("B");
-			System.out.println("Item pushed into stack : "+stack.peek());
+			System.out.println("Item pushed into stack : " + stack.peek());
 			stack.push("C");
-			System.out.println("Item pushed into stack : "+stack.peek());
+			System.out.println("Item pushed into stack : " + stack.peek());
 			stack.push("D");
-			System.out.println("Item pushed into stack : "+stack.peek());
+			System.out.println("Item pushed into stack : " + stack.peek());
 			stack.push("E");
-			System.out.println("Item pushed into stack : "+stack.peek());
-			stack.push("F");	// Will through StackFullException for stack size 5
-			System.out.println("Item pusher into stack : "+stack.peek());
-		}catch(StackFullException stkFullEx){
+			System.out.println("Item pushed into stack : " + stack.peek());
+			stack.push("F"); // Will throw StackFullException for stack size 5
+			System.out.println("Item pusher into stack : " + stack.peek());
+		} catch (StackFullException stkFullEx) {
 			System.out.println(stkFullEx.getMessage());
-		}catch(StackEmptyException stkEmptyEx){
+		} catch (StackEmptyException stkEmptyEx) {
 			System.out.println(stkEmptyEx.getMessage());
 		}
-		
-		try{
-			while(!stack.isEmpty()){
+
+		try {
+			while (!stack.isEmpty()) {
 				System.out.println(stack.peek());
-				System.out.println("Item  popped out of the stack : "+stack.pop());
+				System.out.println("Item  popped out of the stack : " + stack.pop());
 			}
-			System.out.println("Item present at the top of stack : "+stack.peek());
-		}catch(StackEmptyException stkEmptyEx){
+			System.out.println("Item present at the top of stack : " + stack.peek());
+		} catch (StackEmptyException stkEmptyEx) {
 			System.out.println(stkEmptyEx.getMessage());
 		}
 	}
