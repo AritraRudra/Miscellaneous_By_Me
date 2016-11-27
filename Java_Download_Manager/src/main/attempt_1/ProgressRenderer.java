bd1d0a17c5d0d8ed701e13be77df2919dcb9873c
AritraRudra <aritrarudra@gmail.com>
2016-11-27 22:22:40 +0530

    First commit for Java Download Manager created with maven and logging with logback over SLF4J.

---
+++ Java_Download_Manager/src/main/attempt_1/ProgressRenderer.java
@@ -0,0 +1,26 @@
+package main.attempt_1;
+
+import java.awt.Component;
+
+import javax.swing.JProgressBar;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+//This class renders a JProgressBar in a table cell.
+@SuppressWarnings("serial")
+class ProgressRenderer extends JProgressBar implements TableCellRenderer {
+	// Constructor for ProgressRenderer.
+	public ProgressRenderer(int min, int max) {
+		super(min, max);
+	}
+
+	/*
+	 * Returns this JProgressBar as the renderer for the given table cell.
+	 */
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+			int row, int column) {
+		// Set JProgressBar's percent complete value.
+		setValue((int) ((Float) value).floatValue());
+		return this;
+	}
+}
