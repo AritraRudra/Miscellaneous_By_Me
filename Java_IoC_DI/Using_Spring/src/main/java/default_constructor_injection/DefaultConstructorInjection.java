5ed86f2a401cdb3a076a80012422252d9f6ae167
AritraRudra <aritrarudra@gmail.com>
2017-01-01 17:52:01 +0530

    Changes for parameterized constructor, setter, XML reference injections.

--- Java_IoC_DI/Using_Spring/src/main/java/default_constructor_injection/DefaultConstructorInjection.java
+++ Java_IoC_DI/Using_Spring/src/main/java/default_constructor_injection/DefaultConstructorInjection.java
@@ -4,18 +4,18 @@ public class DefaultConstructorInjection {
 	// When the container instantiates the message bean, it is equivalent to
 	// initializing an object in your code with 'new DefaultMessage()'.
 
-	private String messageDefaultConstructorInjection = "Default Constructor Test Using Spring.";
+	private String message = "Default Constructor Test Using Spring.";
 	/**
 	 * Gets message.
 	 */
 	public String getMessage() {
-		return messageDefaultConstructorInjection;
+		return message;
 	}
 
 	/**
 	 * Sets message.
 	 */
 	public void setMessage(String message) {
-		this.messageDefaultConstructorInjection = message;
+		this.message = message;
 	}
 }
--- Java_IoC_DI/Using_Spring/src/main/java/using_spring/App.java
+++ Java_IoC_DI/Using_Spring/src/main/java/using_spring/App.java
@@ -7,6 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import default_constructor_injection.DefaultConstructorInjection;
 import parameterized_constructor_injection.BasicConstructorInjection;
+import setter_injection.BasicSetterInjection;
 
 /**
  * Main starting class
@@ -38,16 +39,24 @@ public class App {
 		logger.info("Spring Application Context Message = "+messageFromAppContext.getMessage());
 		
 		
-		/*
-		// Get Bean with id = message
+		
+		// Get Bean with id = ID_DefaultConstructorInjection
 		DefaultConstructorInjection defaultConstructorInjection 
-			= ( DefaultConstructorInjection) applicationContext.getBean("messageDefaultConstructorInjection");
+			= ( DefaultConstructorInjection) applicationContext.getBean("ID_DefaultConstructorInjection");
 		logger.info(defaultConstructorInjection.getMessage());
 		
-		// Get Bean with id = message
-		BasicConstructorInjection basicConstructorInjection = (BasicConstructorInjection) applicationContext.getBean("message");
+		// Get Bean with id = ID_BasicConstructorInjection
+		BasicConstructorInjection basicConstructorInjection = (BasicConstructorInjection) applicationContext.getBean("ID_BasicConstructorInjection");
 		logger.info(basicConstructorInjection.getMessage());
-		*/
+		
+		// Get Bean with id = ID_BasicSetterInjection
+		BasicSetterInjection basicSetterInjection = (BasicSetterInjection) applicationContext.getBean("ID_BasicSetterInjection");
+		logger.info(basicSetterInjection.getMessage());
+		
+		// Get Bean with id = ID_BasicSetterInjectionXMLRefefence
+		basicSetterInjection = (BasicSetterInjection) applicationContext.getBean("ID_BasicSetterInjectionXMLReference");
+		logger.info(basicSetterInjection.getMessage());
+		
 		((ClassPathXmlApplicationContext)applicationContext).close();
 	}
 
