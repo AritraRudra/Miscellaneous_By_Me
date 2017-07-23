55c669bfde2d7e6eba460b28657ae3ad1c951870
AritraRudra <aritrarudra@gmail.com>
2017-07-24 00:25:46 +0530

    Working JUnit.

--- JDBC/src/test/java/com/dbhandler/dao/EmployeeDAOTest.java
+++ JDBC/src/test/java/com/dbhandler/dao/EmployeeDAOTest.java
@@ -1,16 +1,14 @@
 package com.dbhandler.dao;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.fail;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 
 import javax.naming.NamingException;
 
 import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -21,21 +19,20 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import com.dbhandler.helper.SQLAgent;
 import com.pojo.Employee;
-import com.pojo.Person;
-
 
 @RunWith(MockitoJUnitRunner.class)
 public class EmployeeDAOTest {
-	@Mock
-	private Connection conn;
 	@Mock
 	private SQLAgent sqlAgent;
 	@Mock
 	private EmployeeDAO empDao;
 	@Mock
 	private Employee employee;
-	@Mock
-	private Employee[] employees;
+
+	// Don't mock Object array this way, arrays can't be mocked in this way as they are final.
+	// https://groups.google.com/forum/#!topic/mockito/Dl0Y6xVSFEc
+	//@Mock
+	//private Employee[] employees;// = new Employee[5];
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -50,22 +47,27 @@ public class EmployeeDAOTest {
 		assertNotNull(sqlAgent);
 		assertNotNull(empDao);
 		assertNotNull(employee);
-		assertNotNull(employees);
-		Mockito.when(sqlAgent.getConnection()).thenReturn(conn);
 		/*
 		mockEmployee();
 		mockEmployees();
 		*/
 	}
-	
+	/*
 	private Employee mockEmployee(){
 		return (Mockito.mock(Employee.class));
 	}
-	
+	*/
+	// this also does not work
+	/*
 	private Employee[] mockEmployees(){
 		return (Mockito.mock(Employee[].class));
 	}
-
+	*/
+	
+	private Employee[] mockEmployees(){
+		return (new Employee[5]);
+	}
+	
 	@After
 	public void tearDown() throws Exception {
 	}
@@ -78,17 +80,20 @@ public class EmployeeDAOTest {
 	@Test
 	public void testInsertNewEmployee()throws NamingException, SQLException{
 		mockDBInsert("sqlInsert");
-		Assert.assertEquals(1, empDao.insertNewEmployee(employee));
+		assertEquals(1, empDao.insertNewEmployee(employee));
 	}
 	
 	private void mockDBInsert(final String insertString) throws NamingException, SQLException {
-		Mockito.when(sqlAgent.dbInsert(insertString)).thenReturn(1);
+		// No need for the the below line as it is not known how internally it would work.
+		// We are mocking empDAO class, not SQLAgent class.
+		//Mockito.when(sqlAgent.dbInsert(insertString)).thenReturn(1);
+		Mockito.when(empDao.insertNewEmployee(employee)).thenReturn(1);
 	}
 
 	@Test
 	public void testInsertNewEmployees()throws NamingException, SQLException{
-		mockDBInsert("sqlInsert");
-		Assert.assertEquals(1, empDao.insertNewEmployees(employees));
+		Mockito.when(empDao.insertNewEmployees(mockEmployees())).thenReturn(1);
+		assertEquals(1, empDao.insertNewEmployees(mockEmployees()));
 	}
 
 }
